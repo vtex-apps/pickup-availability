092d6a469d1dd8792e6585361b8e0c55ca77c65b
Breno Calazans <brenocalazans@gmail.com>
2019-06-13 23:21:20 -0300

    Update eslint rules

--- react/typings/storefront.d.ts
+++ react/typings/storefront.d.ts
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactElement } from 'react'
+import { FunctionComponent } from 'react'
 
 declare global {
   interface StorefrontFunctionComponent<P = {}> extends FunctionComponent<P> {
